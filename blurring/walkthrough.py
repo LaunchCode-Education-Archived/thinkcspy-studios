39794cf9dbf5f1a784c07f6fe9eaa967fa929a36
Jesse Levine <jesseilev@users.noreply.github.com>
2016-10-18 13:58:14 -0500

    Update walkthrough.py

--- blurring/walkthrough.py
+++ blurring/walkthrough.py
@@ -1,3 +1,17 @@
-"""
-Walkthrough:
-"""
+import image
+
+img = image.Image("luther.jpg")
+win = imge.ImageWin()
+
+for x in range(img.getWidth()):
+    for y in range(img.getHeight()):
+        p = img.getPixel(x, y)
+        red = p.getRed()
+        green = p.getGreen()
+        blue = p.getBlue()
+        new_p = image.Pixel(255, green, blue)
+        img.setPixel(x, y, new_p)
+        
+img.draw(win)
+win.exitonclick()
+    
