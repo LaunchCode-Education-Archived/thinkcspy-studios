b00f8b9505b4f9f20f7a57776c795513d3e41490
Jesse Levine <jesseilev@users.noreply.github.com>
2016-10-18 14:13:42 -0500

    Update studio.py

--- sorted/studio.py
+++ sorted/studio.py
@@ -1,3 +1,6 @@
-"""
-Studio:
-"""
+def is_sorted(string):
+    """Returns True if string is sorted from least to greatest
+       False otherwise.
+    """
+    # TODO: Fill in details
+    return False
