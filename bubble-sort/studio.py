0fa5cfaba8fd27732e4562287e2ca6b6c93eec76
Jesse Levine <jesseilev@users.noreply.github.com>
2016-10-18 15:01:19 -0500

    Update studio.py

--- bubble-sort/studio.py
+++ bubble-sort/studio.py
@@ -1,3 +1,12 @@
-"""
-Studio:
-"""
+# Sorts a list using bubble sort.
+# Does not alter the original list but returns a sorted version of it.
+def bubble_sort(lst):
+    # TODO your code here
+
+
+
+from test import testEqual
+testEqual(bubble_sort([0]), [0])  # Sorts a single element, returns same list
+testEqual(bubble_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])  # Sorted list is the same
+testEqual(bubble_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+testEqual(bubble_sort([4, 5, 3, 1, 2]), [1, 2, 3, 4, 5])
