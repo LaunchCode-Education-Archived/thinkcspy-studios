6b010db78dd8beab31876928591650a8812f1ec8
Jesse Levine <jesseilev@users.noreply.github.com>
2016-10-18 13:45:44 -0500

    Update studio.py

--- searching/studio.py
+++ searching/studio.py
@@ -1,3 +1,7 @@
-"""
-Studio:
-"""
+def contains_n(numbers, n):
+    # Your code here, to replace this non-working version
+    return False
+
+# Be sure to test your function with various inputs
+
+
